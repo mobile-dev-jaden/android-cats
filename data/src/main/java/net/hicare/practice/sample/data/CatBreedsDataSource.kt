e35da96c4657f53a56a2d5974debd1b2f1044f8d
Taeho-Kim <dev.jaden.kim@gmail.com>
2023-02-18 17:02:40 +0900

    refactor Unused import directive Cat breed data source 수정

--- data/src/main/java/net/hicare/practice/sample/data/CatBreedsDataSource.kt
+++ data/src/main/java/net/hicare/practice/sample/data/CatBreedsDataSource.kt
@@ -1,11 +1,8 @@
 package net.hicare.practice.sample.data
 
 import android.util.Log
-import kotlinx.coroutines.*
 import net.hicare.practice.sample.data.apis.CatBreedApi
 import net.hicare.practice.sample.data.models.response.CatBreed
-import retrofit2.create
-import retrofit2.http.GET
 
 class CatBreedsDataSource: BaseService() {
     companion object {
